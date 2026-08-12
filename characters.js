de65b5c1afe4bf8bb7ba0eb011d4a10cb9b96eb3
tarutarune <gsp.ghd@gmail.com>
2026-08-12 11:39:35 +0900

    Update characters.js

--- characters.js
+++ characters.js
@@ -1052,16 +1052,15 @@ const characters = [
     element: "氷",
     weapon: "両手剣",
     image: "images/Sandrone.png"
-  }
-  
-    {
+  },
+  {
     id: "Alyosha",
     name: "アリョーシャ",
     rarity: 4,
     element: "雷",
     weapon: "長柄武器",
     image: "images/Alyosha.png"
-  }
+  },
 
     {
     id: "Odette",
